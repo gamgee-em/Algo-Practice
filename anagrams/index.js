481ed445081301da628fc3085f741ede3693689b
Sam Sweigart <samuel.sweigart@gmail.com>
2021-12-06 13:56:01 -0500

    anagram character map solution using helper func

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,21 +8,48 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-//* Solution 1
+//* Solution 1 | compare all chars
 /* const anagrams = (stringA, stringB) => {
 
     return stringA.split('').sort().join('').toLowerCase() === stringB.split('').sort().join('').toLowerCase();
 
 }; */
 
-//* Solution 2
+//* Solution 2 | do not compare whitespace or special chars
 
-const anagrams = (stringA, stringB) => {
+/* const anagrams = (stringA, stringB) => {
 
     const regex = /[^\w]/g;
 
-    return (stringA.replace(regex, '').split('').sort().join('').toLowerCase() === stringB.replace(regex, '').split('').sort().join('').toLowerCase());
+    return stringA.replace(regex, '').split('').sort().join('').toLowerCase() === stringB.replace(regex, '').split('').sort().join('').toLowerCase();
 
-}
+} */
+
+//* Solution 3 | char map
+
+const anagrams = (stringA, stringB) => {
+
+    const charMapA = buildCharMap(stringA);
+    const charMapB = buildCharMap(stringB);
+    
+    if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+        return false;
+    };
+    
+    for (let char in charMapA) {
+        if (charMapA[char] !== charMapB[char] && charMapB[char] !== charMapA[char]) {
+            return false;
+        };
+    };
+    return true;
+};
+
+const buildCharMap = (str) => {
+    const charMap = {};
+    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+        charMap[char] = charMap[char] + 1 || 1;
+    };
+    return charMap;
+};
 
 module.exports = anagrams;
