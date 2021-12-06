897a136e314d3188d388f5d12acdc565ca856386
Sam Sweigart <samuel.sweigart@gmail.com>
2021-12-06 17:06:38 -0500

    steps solution 1

--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,20 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+//* Solution 1
+const steps = (n) => {
+    for (let i = 0; i < n; i++) {
+        let stair = '';
+
+        for (let j = 0; j < n; j++) {
+            if (j <= i) {
+                stair += '#';
+            } else {
+                stair += ' ';
+            };
+        };
+        console.log(stair);
+    };
+};
 
 module.exports = steps;
