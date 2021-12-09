8a7ea64676a4f4ec4f8b2733f486abf345a3a4e2
Sam Sweigart <samuel.sweigart@gmail.com>
2021-12-09 17:44:47 -0500

    iterative solution

--- pyramid/index.js
+++ pyramid/index.js
@@ -13,7 +13,29 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//       '   #   '
+//       '  ###  '
+//       ' ##### '
+//       '#######'
 
-function pyramid(n) {}
+//* Solution 1 | iterative
+const pyramid = (n) => {
+    const mid = Math.floor((2 * n - 1) / 2);
+
+    for (let i = 0; i < n; i++) {
+        let level = '';
+        
+        for (let j = 0; j < 2 * n - 1; j++) {
+            if (mid - i <= j && mid + i >= j) {
+                level += '#';
+            } else {
+                level += ' ';
+            }
+        }
+        console.log(level);
+    }
+};
+
 
 module.exports = pyramid;
