c863d6bded07f8f08cba4690d7248be08181b979
Sam Sweigart <samuel.sweigart@gmail.com>
2022-01-21 11:11:41 -0500

    added runtime explanations

---
+++ big_o/big_o.js
@@ -0,0 +1,13 @@
+//* Using Big O to Compare Algorithms
+
+// O(1) --> Constant Time Complexity 
+// n (number of input) has no effect on the time the algorithm takes
+
+// O(log n) --> Logarithmic Time Complexity
+// Execution time grows logarithmaically with n
+
+// O(n) --> Linear Time Complexity
+// Execution time grows linearly with n
+
+// O(n2) --> Quadratic Time Complexity
+// Execution time grows quadratically with n
