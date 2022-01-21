436edde063afdcda124e2cf3ad78fadd6de7d99e
Sam Sweigart <samuel.sweigart@gmail.com>
2022-01-21 11:13:36 -0500

    added comments

--- fib/index.js
+++ fib/index.js
@@ -8,52 +8,59 @@
 // Example:
 //   fib(4) === 3
 
-// iterative solution
-// linear runtime
-/* const fib = n => {
+//* iterative solution
+//! linear runtime
+const fib = n => {
     let fibArr = [0, 1];
 
-    for(let i = 2; i <= n; i++) {
+    for (let i = 2; i <= n; i++) {
         const a = fibArr[fibArr.length - 1];
         const b = fibArr[fibArr.length - 2];
         fibArr.push(a + b);
     };
     return fibArr[n];
-}; */
+};
 
-// recursive solution
-// exponential runtime
-
-const fib = n => {
+//* recursive solution
+//! exponential runtime
+/* const fib = n => {
     if (n < 2) {
         return n;
     };
 
     return fib(n - 1) + fib(n - 2);
-};
+}; */
 
+//* recursive solution
 //! one liner
 // const fib = n => n < 2 ? n : fib( n - 1 ) + fib( n - 2 );
 
-// memoization store results of previous function calls
+//* memoization store results of previous function calls
+//! by caching previous function calls we speed up execution significantly
 /* const memoize = fn => {
     
     const cache = {};
 
     return ((...args) => {
+
         if (cache[args]) return cache[args];
+
         const result = fn.apply(this, args);
         cache[args] = result;
+
         return result;
+
     });
 };
 
 const fib = n => {
+    
     if (n < 2) {
         return n;
     };
 
     return fib(n - 1) + fib(n - 2)
+
 };
 
 fib = memoize(fib); */
